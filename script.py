a8b1c02112b24b47a8d9df10ce966232e37e6673
Akim Malyschik <akimich11@yandex.by>
2021-08-24 19:25:39 +0300

    added startup checking

--- script.py
+++ script.py
@@ -10,11 +10,14 @@ bot = telebot.TeleBot(token='1934090305:AAEQVPnZer-7TBMEwbTW_1n3pS3PBBELcmg')
 
 if __name__ == '__main__':
 	bot.send_message(270241310, 'started')
+	while True:
+		check_timetable(-452095269, False)
+		sleep(60)
 
 
-def check_timetable(message, manual_check=True):
+def check_timetable(chat_id, manual_check=True):
 	if manual_check:
-		bot.send_message(message.chat.id, 'ща проверю')
+		bot.send_message(chat_id, 'ща проверю')
 	driver = webdriver.PhantomJS()
 	driver.get('http://fpmi.bsu.by/ru/main.aspx?guid=20381')
 	try:
@@ -24,11 +27,11 @@ def check_timetable(message, manual_check=True):
 		found = False
 		for ref in refs:
 			if ref.text.lower().startswith('расписан'):
-				bot.send_message(message.chat.id, ref.get_property('href'))
+				bot.send_message(chat_id, ref.get_property('href'))
 				found = True
 				break
 		if not found and manual_check:
-			bot.send_message(message.chat.id, 'пока расписания нет')
+			bot.send_message(chat_id, 'пока расписания нет')
 	except WebDriverException:
 		bot.send_message(270241310, 'Беда с селениумом:\n' + traceback.format_exc())
 	driver.quit()
@@ -36,7 +39,7 @@ def check_timetable(message, manual_check=True):
 
 @bot.message_handler(commands=['check'])
 def check_response(message):
-	check_timetable(message)
+	check_timetable(message.chat.id)
 
 
 @bot.message_handler(commands=['start_checking'])
@@ -44,7 +47,7 @@ def start_checking(message):
 	if message.from_user.id == 270241310:
 		bot.send_message(message.chat.id, 'буду чекать каждые 60 секунд, если появится - скину')
 		while True:
-			check_timetable(message, False)
+			check_timetable(message.chat.id, False)
 			sleep(60)
 
 
